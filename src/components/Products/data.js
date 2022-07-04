99e6a4f18ee891309775b0e66a1334e52c7118ef
NishankBabu <78193025+NishankBabu@users.noreply.github.com>
2022-07-04 22:54:21 +0530

    Add files via upload

---
+++ src/components/Products/data.js
@@ -0,0 +1,66 @@
+import product1 from '../../images/product-1.png';
+import product2 from '../../images/product-2.png';
+import product3 from '../../images/product-3.png';
+import salad1 from '../../images/salad-1.png';
+import salad2 from '../../images/salad-2.png';
+import salad3 from '../../images/salad-3.png';
+
+export const productData = [
+  { 
+    img: product1,
+    alt: 'Burger',
+    name: 'Dave-Single',
+    desc:
+      'A quarter-pound* of fresh British beef, American cheese, crisp lettuce, tomato, pickle, ketchup, mayo, and onion on a toasted bun just the way Dave intended.',
+    price: '200 ₹',
+    button: 'Add to Cart'
+  },
+  {
+    img: product2,
+    alt: 'Burger',
+    name: 'Dave-Double',
+    desc:
+      ' A half-pound* of fresh British beef, American cheese, crisp lettuce, tomato, pickle, ketchup, mayo, and onion on a toasted bun. More meat means more to love.',
+    price: '400 ₹',
+    button: 'Add to Cart'
+  },
+  {
+    img: product3,
+    alt: 'Burger',
+    name: 'Dave-triple',
+    desc:
+      ' Three quarters of a pound* of fresh British beef, American cheese, crisp lettuce, tomato, pickle, ketchup, mayo, and onion on a toasted bun. Use both hands!',
+    price: '700 ₹',
+    button: 'Add to Cart'
+  }
+];
+
+export const productDataTwo = [
+  {
+    img: salad1,
+    alt: 'salad',
+    name: 'DCaesar Chicken Salad',
+    desc:
+      'Made fresh daily with romaine lettuce, grilled chicken breast, Italian cheese, crunchy croutons, and creamy Caesar dressing. One bite will tell you why its king of more than just the Romaines.',
+    price: '150 ₹',
+    button: 'Add to Cart'
+  },
+  {
+    img: salad2,
+    alt: 'salad',
+    name: 'Apple Cranberry Chicken Salad',
+    desc:
+      'Made fresh daily with our signature lettuce blend, crisp apple chips, dried cranberries, sunflower seeds, Italian cheese and grilled chicken breast hot off the grill, all topped with a Pomegranate Vinaigrette Dressing. An unbeatable pick.',
+    price: '500 ₹',
+    button: 'Add to Cart'
+  },
+  {
+    img: salad3,
+    alt: 'salad',
+    name: 'Avocado Chicken Salad',
+    desc:
+      'Made fresh daily with our signature lettuce blend, cheddar cheese, diced tomatoes, cool, creamy avocado, Applewood smoked bacon, and grilled chicken breast hot off the grill, all topped with a Southwest Ranch Dressing.',
+    price: '1000 ₹',
+    button: 'Add to Cart'
+  }
+];
